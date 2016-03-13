c15a6333156664d250e08587bfca43cb584d8a2d
rozaki <eleftheria.rozi@gmail.com>
2016-03-14 00:16:36 +0100

    Hero OBJECT. Hero Interface instead of class

--- app/app.component.js
+++ app/app.component.js
@@ -21,12 +21,15 @@ System.register(['angular2/core'], function(exports_1, context_1) {
             AppComponent = (function () {
                 function AppComponent() {
                     this.title = 'Tour of Heroes';
-                    this.heroe = 'Windstorm';
+                    this.hero = {
+                        id: 1,
+                        name: 'Windstorm'
+                    };
                 }
                 AppComponent = __decorate([
                     core_1.Component({
                         selector: 'my-app',
-                        template: '<h1>{{title}}</h1><h2>{{heroe}} details!</h2>'
+                        template: '<h1>{{title}}</h1><h2>{{hero.name}} details!</h2>'
                     }), 
                     __metadata('design:paramtypes', [])
                 ], AppComponent);
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,17 @@
 import {Component} from 'angular2/core';
+interface Hero {
+  id: number;
+  name: string;
+}
 
 @Component({
     selector: 'my-app',
-    template: '<h1>{{title}}</h1><h2>{{heroe}} details!</h2>'
+    template: '<h1>{{title}}</h1><h2>{{hero.name}} details!</h2>'
 })
 export class AppComponent {
     public title = 'Tour of Heroes';
-    public heroe = 'Windstorm';
+    public hero: Hero = {
+        id: 1,
+        name: 'Windstorm'
+    };
  }
