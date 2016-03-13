f4a4b2e3d59555e6238d10e0c5f49a3c851c99b6
rozaki <eleftheria.rozi@gmail.com>
2016-03-14 00:34:46 +0100

    -multi-line template using ES2015’s template strings - two-way data binding to the <input> element using the built-in ngModel directive.

--- app/app.component.js
+++ app/app.component.js
@@ -29,7 +29,7 @@ System.register(['angular2/core'], function(exports_1, context_1) {
                 AppComponent = __decorate([
                     core_1.Component({
                         selector: 'my-app',
-                        template: '<h1>{{title}}</h1><h2>{{hero.name}} details!</h2>'
+                        template: "\n      <h1>{{title}}</h1>\n      <h2>{{hero.name}} details!</h2>\n      <div><label>id: </label>{{hero.id}}</div>\n      <div>\n        <label>name: </label>\n        <div><input [(ngModel)]=\"hero.name\" placeholder=\"name\"></div>\n      </div>\n      "
                     }), 
                     __metadata('design:paramtypes', [])
                 ], AppComponent);
--- app/app.component.ts
+++ app/app.component.ts
@@ -6,7 +6,15 @@ interface Hero {
 
 @Component({
     selector: 'my-app',
-    template: '<h1>{{title}}</h1><h2>{{hero.name}} details!</h2>'
+    template: `
+      <h1>{{title}}</h1>
+      <h2>{{hero.name}} details!</h2>
+      <div><label>id: </label>{{hero.id}}</div>
+      <div>
+        <label>name: </label>
+        <div><input [(ngModel)]="hero.name" placeholder="name"></div>
+      </div>
+      `
 })
 export class AppComponent {
     public title = 'Tour of Heroes';
